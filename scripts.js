2011e52b74f5cbbbb6f9fdb55fd22c7461c7e86a
Yahya Hussein <yahyahussein91@gmail.com>
2015-10-11 20:33:50 -0700

    made the pics hidden on page load

---
+++ scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	console.log('yay it works');
+
+
+	//code to prevent page from refreshing when submit button is pressed
+	$('.submit').click(function(event){
+		event.preventDefault();
+
+	});
+
+
+});
