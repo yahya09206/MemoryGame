e50457612968a3aa35636114af1b5f0550f28b46
Yahya Hussein <yahyahussein91@gmail.com>
2017-05-31 14:57:35 -0700

    added array of gifs

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
 //Make game using vanilla javascript
 
 //Variables needed for game
-var playerOne,playerTwo,activePlayer,Clicks;
+var playerOne, playerTwo, numClicks, imageOne, imageTwo, matches;
+
+playerOne;
+playerTwo;
+imageOne;
+imageTwo;
+clicks = 0;
+matches = 0;
+
+//Array of gifs
+//array with the url's of the gifs
+	var url = [ 
+	
+		"http://i1345.photobucket.com/albums/p670/JLe10371037/Samuel-L-Jackson-baby-out-of-my-face-spike-awards-1354071672z_zps8b119d51.gif",
+		"http://i1345.photobucket.com/albums/p670/JLe10371037/Samuel-L-Jackson-baby-out-of-my-face-spike-awards-1354071672z_zps8b119d51.gif",
+
+		"http://i.imgur.com/Z8mGjEk.gif",
+		"http://i.imgur.com/Z8mGjEk.gif",
+
+		"http://24.media.tumblr.com/176122479cd4656568b3f58a5cd72f3c/tumblr_mkhmtb0idq1s0uuv7o6_r1_250.gif",
+		"http://24.media.tumblr.com/176122479cd4656568b3f58a5cd72f3c/tumblr_mkhmtb0idq1s0uuv7o6_r1_250.gif",
+
+		"http://25.media.tumblr.com/tumblr_m3je3fYHhb1r5r8duo1_250.gif",
+		"http://25.media.tumblr.com/tumblr_m3je3fYHhb1r5r8duo1_250.gif",
+
+		"http://38.media.tumblr.com/tumblr_lkqyd0GGHE1qc3n92o1_500.gif",
+		"http://38.media.tumblr.com/tumblr_lkqyd0GGHE1qc3n92o1_500.gif",
+
+		"http://media.tumblr.com/tumblr_lo2tyrWh8o1qfeod9.gif",
+		"http://media.tumblr.com/tumblr_lo2tyrWh8o1qfeod9.gif"
+];
